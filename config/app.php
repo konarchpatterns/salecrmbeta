99984c9bb45376ec43d743a8b1b6c50c851bc4cc
Konarch Patterns247 <konarch@patterns247.net>
2024-03-07 23:30:24 +0530

    new changes

--- config/app.php
+++ config/app.php
@@ -56,7 +56,7 @@ return [
 
     'url' => env('APP_URL', 'https://konarch.in/'),
 
-    'asset_url' => env('ASSET_URL', 'https://konarch.in/ftpconnection/public/'),
+    'asset_url' => env('ASSET_URL', null),
 
     /*
     |--------------------------------------------------------------------------
